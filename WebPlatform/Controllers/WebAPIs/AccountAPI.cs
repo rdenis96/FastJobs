c8dd3d43b21ff3c65a1d6779021557c411a8e95d
Denis <rdenis9616@gmail.com>
2018-06-28 06:34:26 +0300

    Create User worker and AccountAPI class

---
+++ WebPlatform/Controllers/WebAPIs/AccountAPI.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebPlatform.Models;
+using WebPlatform.Resources.Helpers;
+using WebPlatform.Workers;
+
+namespace WebPlatform.Controllers.WebAPIs
+{
+    [RoutePrefix("api/account")]
+    public class AccountAPI : ApiController
+    {
+        private UserWorker userWorker = new UserWorker();
+
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        [Route(Name = "getAllUsers")]
+        [HttpGet]
+        public IHttpActionResult GetAll(int id)
+        {
+            ICollection<User> users = userWorker.GetAll();
+            return Ok(users);
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebPlatform/Workers/UserWorker.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebPlatform.DataLayer.Repository.Implementations;
+using WebPlatform.DataLayer.Repository.Interfaces;
+using WebPlatform.Models;
+
+namespace WebPlatform.Workers
+{
+    public class UserWorker
+    {
+        private IUserRepository userRepository;
+
+        public UserWorker()
+        {
+            userRepository = new UserRepository();
+        }
+
+        public User GetByEmail(string email)
+        {
+            return userRepository.GetByEmail(email);
+        }
+
+        public User GetByPhone(string phone)
+        {
+            return userRepository.GetByPhone(phone);
+        }
+
+        public ICollection<User> GetByFirstname(string firstname)
+        {
+            return userRepository.GetByFirstname(firstname);
+        }
+
+        public ICollection<User> GetByLastname(string lastname)
+        {
+            return userRepository.GetByLastname(lastname);
+        }
+
+        public ICollection<User> GetByIp(string ip)
+        {
+            return userRepository.GetByIp(ip);
+        }
+
+        public ICollection<User> GetAllOnlineBetweenDates(DateTime startDate, DateTime endDate)
+        {
+            return userRepository.GetAllOnlineBetweenDates(startDate, endDate);
+        }
+
+        public ICollection<User> GetAllRegisteredBetweenDates(DateTime startDate, DateTime endDate)
+        {
+            return userRepository.GetAllRegisteredBetweenDates(startDate, endDate);
+        }
+
+        public User Create(User obj)
+        {
+            return userRepository.Create(obj);
+        }
+
+        public User Update(User obj)
+        {
+            return userRepository.Update(obj);
+        }
+
+        public bool Delete(User obj)
+        {
+            return userRepository.Delete(obj);
+        }
+
+        public User GetById(int id)
+        {
+            return userRepository.GetById(id);
+        }
+
+        public ICollection<User> GetAll()
+        {
+            return userRepository.GetAll();
+        }
+    }
+}
